34fb90c412ce06cf017619e4c20413f0a897a3d0
ndejesusmartinez <nmartinezc97@hotmail.com>
2022-03-15 20:24:11 -0500

    modificaciones

--- admin-public/admin.php
+++ admin-public/admin.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Admin || Hotel Sol Del Mar</title>
+    <link rel="stylesheet" href="../estilos.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   </head>
@@ -10,7 +11,7 @@
   <div class="container">
     <nav class="navbar navbar-dark bg-dark">
       <div class="container-fluid">
-      <a class="navbar-brand" href="./admin.html">HOTEL SOL DEL MAR</a>
+      <a class="navbar-brand" href="./admin.php">HOTEL SOL DEL MAR</a>
       <a class="navbar-brand" href="./reservas.html">REALIZAR RESERVAS</a>
       <a class="navbar-brand" href="./consultas.html">CONSULTAR RESERVAS</a>
       <a class="navbar-brand" href="../login.html">SALIR</a>
@@ -21,7 +22,40 @@
       </div>
     </nav>
 
+    <div class="usuarios">
+      <h2>usuarios activos en la base de datos</h2>
+      <div class="cont">
+
+      <?php
+      require "../php/conexion.php";
+      $sql="SELECT * FROM usuarios";
+      $resultado=$conn->query($sql);
+
+      ?>
+       <table class="tabla-usuarios">
+           <tr>
+                <td><center>ID</center></td>
+                <td><center>USUARIO</center></td>
+                <td><center>PASSWORD</center></td>
+           </tr>
+
+       <?php
+      while ($datos=$resultado->fetch_array())
+       {
+       ?>
+       <tr>
+       <td><center><?php echo $datos['id']?><center></td>
+       <td><center><?php echo $datos['usuario']?></center></td>
+       <td><center><?php echo $datos['password']?><center></td>
+      </tr>
+      <?php
+       }
+      ?>
+        </table>
+    </div>
+    </div>
   </div>
+
 
 
   </body>
